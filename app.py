cdc493598670f8e9bcd7faca8b3d9d7440a0cb1d
napoerreape121 <napoerreape@gmail.com>
2026-05-12 00:55:52 -0300

    app.py

--- app.py
+++ app.py
@@ -3,13 +3,31 @@ import pandas as pd
 import ta
 import streamlit as st
 
-# Lista de CEDEARs / acciones
+# Lista completa de CEDEARs (extraída del PDF)
 tickers = [
-    "AAPL", "ADBE", "AMD", "AMZN", "BA", "BABA", "BBD", "BIDU", "C", "CAT",
-    "COIN", "CRM", "CVX", "DIS", "GE", "GOOGL", "GOLD", "HD", "IBM", "INTC",
-    "JNJ", "JPM", "KO", "MA", "MCD", "MELI", "META", "MSFT", "NFLX", "NVDA",
-    "PFE", "PYPL", "QCOM", "SHOP", "SPOT", "T", "TSLA", "V", "VALE",
-    "VZ", "WMT", "XOM", "AAL", "ABEV", "ABT", "ACN", "ARKK", "AMGN", "EBAY"
+    "AAL","AALC","AALD","AAP","AAPC","AAPD","AAPL","AAPLC","AAPLD","ABBV","ABBVC","ABBVD",
+    "ABEV","ABEV3","ABEVC","ABEVD","ABNB","ABNBC","ABNBD","ABT","ABTC","ABTD","ACN","ACNC","ACND",
+    "ACWI","ACWIC","ACWID","ADBE","ADBEC","ADBED","ADGO","ADGOC","ADGOD","ADI","ADID","ADP","ADS",
+    "ADSC","ADSD","AEG","AEM","AEMC","AEMD","AI","AIC","AID","AIG","AIGC","AIGD","AKO.B","ALAB",
+    "ALAC","ALAD","AMAT","AMATC","AMATD","AMD","AMDC","AMDD","AMGN","AMGNC","AMGND","AMX","AMXC",
+    "AMXD","AMZN","AMZNC","AMZND","ANF","ANFC","ANFD","ARCO","ARCOC","ARCOD","ARKK","ARKKC","ARKKD",
+    "ARM","ARMD","ASML","ASMLC","ASMLD","ASR","ASTS","ASTSC","ASTSD","AVGO","AVGOC","AVGOD","AVY",
+    "AVYC","AVYD","AXIA","AXIAC","AXIAD","AXP","AXPC","AXPD","AZN","AZNC","AZND","B","B.C","B.D",
+    "BA","BA.C","BA.CC","BA.CD","BABA","BABAC","BABAD","BAC","BAD","BAK","BAKC","BAKD","BAS","BAYN",
+    "BB","BBA3C","BBA3D","BBAS3","BBD","BBDC3","BBDCC","BBDCD","BBDD","BBV","BBVD","BCS","BHP","BHPD",
+    "BIDU","BIDUC","BIDUD","BIIB","BIIBC","BIIBD","BIOX","BIOXC","BIOXD","BITF","BITFC","BITFD","BK",
+    "BKC","BKD","BKNG","BKNGC","BKNGD","BKR","BKRC","BKRD","BMNR","BMNRC","BMNRD","BMY","BNG","BNGC",
+    "BNGD","BP","BPA11","BPA1C","BPA1D","BPC","BPD","BRKB","BRKBC","BRKBD","BSBR","BX","BXC","BXD",
+    "C","C.D","CAAP","CAAPC","CAAPD","CAH","CAHC","CAHD","CAR","CAR.C","CAR.D","CAT","CATC","CATD",
+    "CC","CCL","CCLC","CCLD","CDE","CEG","CEGC","CEGD","CIBR","CIBRC","CIBRD","CL","CLC","CLD","CLS",
+    "CLSC","CLSD","COIN","COINC","COIND","COPX","COPXC","COPXD","COST","COSTC","COSTD","CRM","CRMC",
+    "CRMD","CRWV","CRWVC","CRWVD","CSCO","CSCOC","CSCOD","CSNA3","CSNAC","CSNAD","CVS","CVSC","CVSD",
+    "CVX","CVXC","CVXD","CX","DAL","DALC","DALD","DD","DE","DEC","DECK","DECKC","DECKD","DED","DEO",
+    "DEOC","DEOD","DHR","DHRC","DHRD","DIA","DIAC","DIAD","DISN","DISNC","DISND","DJN3C","DJN3D",
+    "DJNJ3","DOCU","DOCUC","DOCUD","DOW","DOWC","DOWD","E","EA","EAC","EAD","EBAY","EBAYC","EBAYD",
+    "EC","ECL","ECLC","ECLD","EEM","EEMC","EEMD","EFA","EFAC","EFAD","EFX","EFXC","EFXD","ELP","ELPC",
+    "ELPCC","ELPCD","ELPD","EMBJ","EMBJC","EMBJD","EOAN","EOANC","EQNR","EQNRC","EQNRD","ERIC","ESGU",
+    "ESGUC","ESGUD","ETHA","ETHAC","ETHAD","ETSY","ETSYC","ETSYD","EWJ","EWJC","EWJD","EWY"
 ]
 
 def scanner(ticker):
@@ -51,3 +69,4 @@ if resultados:
         st.write(r)
 else:
     st.write("No se detectaron señales en este momento.")
+
